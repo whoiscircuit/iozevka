c8f1244c2f2bb8827ee6ecc240f3797dbc155219
circuit <oxcl@proton.me>
2025-05-17 06:49:02 +0330

    add hash codes for the font derivations

--- flake.nix
+++ flake.nix
@@ -9,13 +9,13 @@
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
-    mkIozevka = {variant,hash}: pkgs.stdenvNoCC.mkDerivation rec {
+    mkIozevka = {variant,hash,filename}: pkgs.stdenvNoCC.mkDerivation rec {
       pname = "iozevka-${variant}";
       version = "1.2";
       propagatedUserEnvPkgs = [];
       src = pkgs.fetchzip {
-        url = "https://github.com/oxcl/iozevka/releases/download/v${version}/iozevka-${variant}.zip";
-        hash = "";
+        inherit hash;
+        url = "https://github.com/whoiscircuit/iozevka/releases/download/v${version}/${filename}.zip";
       };
       installPhase = ''
         runHook preInstall
@@ -23,21 +23,19 @@
         runHook postInstall
       '';
     };
-    iozevka = mkIozevka {variant = "all"; hash = "";};
-    iozevka-code = mkIozevka {variant = "code"; hash = "";};
-    iozevka-nerd = mkIozevka {variant = "nerd"; hash = "";};
-    iozevka-mono = mkIozevka {variant = "mono"; hash = "";};
-    iozevka-term = mkIozevka {variant = "term"; hash = "";};
-    iozevka-quasi = mkIozevka {variant = "quasi"; hash = "";};
-    iozevka-slabs = mkIozevka {variant = "slabs"; hash = "";};
+    iozevka-code  = mkIozevka {variant = "code" ; filename="IoZevkaCode"; hash = "sha256-vrMW9nDTT0z+R5IezKm/xPLRDNEX32nW67jGirBg3pM=";};
+    iozevka-nerd  = mkIozevka {variant = "nerd" ; filename="IoZevkaNerd"; hash = "sha256-edCXj5/d+fqJBGTZDHFiGFuy7XJdPtu/AnyD1MbkFZU=";};
+    iozevka-mono  = mkIozevka {variant = "mono" ; filename="IoZevkaMono"; hash = "sha256-LVvbMqqQ1qEQ8XPrWLR5FD1tuIyB2s4Rub1BWqcWqus=";};
+    iozevka-term  = mkIozevka {variant = "term" ; filename="IoZevkaTerm"; hash = "sha256-CUbnufQbF4ayiXTLmXUAKNJhFzlr2WqFzcdzhx8zR3s=";};
+    iozevka-quasi = mkIozevka {variant = "quasi"; filename="IoZevkaQuasi"; hash = "sha256-l6AfaEskceFFR83RirxfmaVuy/nfKeCcmCKwmo/glCQ=";};
+    iozevka-slabs = mkIozevka {variant = "slabs"; filename="IoZevkaSlabs"; hash = "sha256-LyeRWe1SIto6lBjhhJdFEVD1EwbLY1UcqDclwQjUM5c=";};
     iozevka-git = import ./build.nix pkgs;
   in {
     packages.${system} = {
-      inherit iozevka iozevka-git iozevka-nerd iozevka-code iozevka-quasi iozevka-slabs iozevka-term iozevka-mono;
-      default = iozevka;
+      inherit iozevka-git iozevka-nerd iozevka-code iozevka-quasi iozevka-slabs iozevka-term iozevka-mono;
     };
     overlays.default = final: prev: {
-      inherit iozevka iozevka-nerd iozevka-code iozevka-quasi iozevka-slabs iozevka-term iozevka-mono;
+      inherit iozevka-nerd iozevka-code iozevka-quasi iozevka-slabs iozevka-term iozevka-mono;
     };
   };
 }
